615d7ce59e5f7aacdd5a384ec9a6cd31bd080d7d
nastyaoliynuk <144141117+nastyaoliynuk@users.noreply.github.com>
2024-11-28 20:48:07 +0200

    Add files via upload

---
+++ CulcTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CulcTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CulcTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac74ba76-f7ab-470e-bfa1-52a667e6ff3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CulcTest/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcClassBr;
+
+namespace CulcTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMult_ValidValues()
+        {
+            // Тест множення в межах int
+            long a = 20000;
+            long b = 30000;
+            int expected = 20000 * 30000;
+
+            // Виклик методу Mult з класу CalcClass
+            int result = CalcClass.Mult(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void TestMult_InvalidValues()
+        {
+            // Тест множення в межах int
+            long a = 20000;
+            long b = 30000;
+            int expected = 20000 * 30000;
+
+            // Виклик методу Mult з класу CalcClass
+            int result = CalcClass.Mult(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMult_OutOfRangeInput()
+        {
+            // Тест, коли одне з значень виходить за межі int
+            long a = (long)int.MaxValue + 1;
+            long b = 2;
+
+            // Виклик методу Mult з класу CalcClass
+            CalcClass.Mult(a, b);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMult_OutOfRangeResult()
+        {
+            // Тест, коли результат множення виходить за межі int
+            long a = int.MaxValue;
+            long b = 2;
+
+            // Виклик методу Mult з класу CalcClass
+            CalcClass.Mult(a, b);
+        }
+
+        [TestMethod]
+        public void TestMult_ValidEdgeCase()
+        {
+            // Тест на граничних значеннях
+            long a = int.MaxValue;
+            long b = 1;
+            int expected = int.MaxValue;
+
+            // Виклик методу Mult з класу CalcClass
+            int result = CalcClass.Mult(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+
+    }
+}
+
+
